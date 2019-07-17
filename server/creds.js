4074258898f230c28d0bfa27ca2cc43305cd1d6e
Ismael Sanchez <isanchez@mserp.com>
2019-07-17 04:42:00 -0700

    updated

---
+++ server/creds.js
@@ -0,0 +1,5 @@
+const username = "Ish";
+const password = "test123";
+
+module.exports.username = username;
+module.exports.password = password;
--- server/server.js
+++ server/server.js
@@ -3,19 +3,22 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const creds = require('./creds')
+const username = creds.username;
+const password = creds.password;
 
 
 const app = express();
 
 app.use(cors())
 
-mongoose.connect('mongodb://<username>:<password>@ds251632.mlab.com:51632/gql_ninja', { useNewUrlParser: true });
+mongoose.connect(`mongodb://${username}:${password}@ds251632.mlab.com:51632/gql_ninja`, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to the database')
 })
 
 app.listen(4001, ()=>{
-    'You are now about to witness the power of street knowledge'
+    console.log('You are good to go, on http://localhost:4001')
 })
 
 app.use('/graphql', graphqlHTTP({
